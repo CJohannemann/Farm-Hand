5101e55e7a735a1f954631daeb23bb98283e379d
cjohannemann <cjjohannemann@gmail.com>
2024-09-18 18:07:37 -0400

    imported router i created?

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
+import createRouter from './router/router.ts';
 import PrimeVue from 'primevue/config';
 
 
-createApp(App).use(router).use(PrimeVue).mount('#app')
+createApp(App).use(router).use(createRouter).use(PrimeVue).mount('#app')
