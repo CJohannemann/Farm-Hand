60cc366f9742d2890a1b4779e85014d857003fe7
cjohannemann <cjjohannemann@gmail.com>
2024-09-18 17:59:09 -0400

    moved style and updated footer script setup

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,11 @@
     <Footer />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import Footer from "./components/Footer.vue";
+<script setup lang="ts">
 import Header from './components/Header.vue';
-
-export default defineComponent({
-  name: "App",
-  components: {
-    Header,
-    Footer,
-  },
-});
+import Footer from './components/Footer.vue';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+
 </style>
